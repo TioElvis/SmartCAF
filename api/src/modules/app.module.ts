5b6e185fe1ff732831bc5c484a32df63dd2a0098
TioElvis <elvis.veramijares@gmail.com>
2025-04-03 20:13:11 +0200

    Add Mongoose integration

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (_ConfigService_: ConfigService) => ({
+        uri: _ConfigService_.get<string>('DB_URI'),
+      }),
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
