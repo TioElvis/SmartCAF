26a39f4c1e01823a67e37619749b2d547f950fce
TioElvis <elvis.veramijares@gmail.com>
2025-04-03 20:03:20 +0200

    Initialize project structure

---
+++ api/src/main.ts
@@ -0,0 +1,17 @@
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.listen(process.env.PORT ?? 9000);
+}
+
+void bootstrap();
