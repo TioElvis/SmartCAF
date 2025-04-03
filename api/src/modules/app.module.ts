1a5f0907004121820810efa4b0de2ad5f8a6c74d
TioElvis <elvis.veramijares@gmail.com>
2025-04-03 20:27:39 +0200

    Add JWT and Passport modules for authentication

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
@@ -13,6 +15,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         uri: _ConfigService_.get<string>('DB_URI'),
       }),
     }),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (_ConfigService_: ConfigService) => ({
+        secret: _ConfigService_.get<string>('JWT_SECRET'),
+      }),
+    }),
+    PassportModule,
   ],
   controllers: [AppController],
 })
