b2259fbc2f834f859ff20ef0b0868181db10ec2b
TioElvis <elvis.veramijares@gmail.com>
2025-04-03 20:23:12 +0200

    Add Fastify cookie support, enable CORS and validation pipes

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,13 +3,23 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { NestFactory } from '@nestjs/core';
+import FastifyCookie from '@fastify/cookie';
 import { AppModule } from './modules/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  // Config
+  app.enableCors({
+    origin: process.env.BASE_CLIENT_URL,
+    credentials: true,
+  });
+  await app.register(FastifyCookie);
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT ?? 9000);
 }
